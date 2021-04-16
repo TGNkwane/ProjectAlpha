25b8c87b9e533b1d933c751cb9fa1e162ca57b87
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-04-16 17:36:34 +0200

    Incident Form Added And Working

---
+++ PremierSolutions/PremierSolutions/Presentation/Incident.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace PremierSolutions
+{
+    partial class frmIncident
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbReportInfo = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpDateIssued = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbxPriority = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtClientID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCallerID = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCreateIncident = new System.Windows.Forms.Button();
+            this.btnNextForm = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(431, 11);
+            this.lblHeading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(187, 54);
+            this.lblHeading.TabIndex = 0;
+            this.lblHeading.Text = "Incident";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Report Info:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // rtbReportInfo
+            // 
+            this.rtbReportInfo.Location = new System.Drawing.Point(199, 101);
+            this.rtbReportInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rtbReportInfo.Name = "rtbReportInfo";
+            this.rtbReportInfo.Size = new System.Drawing.Size(813, 195);
+            this.rtbReportInfo.TabIndex = 2;
+            this.rtbReportInfo.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 313);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date Issued:";
+            // 
+            // dtpDateIssued
+            // 
+            this.dtpDateIssued.Location = new System.Drawing.Point(199, 316);
+            this.dtpDateIssued.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpDateIssued.Name = "dtpDateIssued";
+            this.dtpDateIssued.Size = new System.Drawing.Size(265, 22);
+            this.dtpDateIssued.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 357);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cost:";
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(199, 364);
+            this.txtCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(265, 22);
+            this.txtCost.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 389);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Priority:";
+            // 
+            // cbxPriority
+            // 
+            this.cbxPriority.FormattingEnabled = true;
+            this.cbxPriority.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.cbxPriority.Location = new System.Drawing.Point(199, 396);
+            this.cbxPriority.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxPriority.Name = "cbxPriority";
+            this.cbxPriority.Size = new System.Drawing.Size(265, 24);
+            this.cbxPriority.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(199, 448);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(177, 146);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Skills";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(8, 108);
+            this.radioButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(60, 21);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "skill4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(8, 80);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(60, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "skill3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 52);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "skill2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 23);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(60, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "skill1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 448);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ability Req:";
+            // 
+            // txtClientID
+            // 
+            this.txtClientID.Location = new System.Drawing.Point(199, 619);
+            this.txtClientID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtClientID.Name = "txtClientID";
+            this.txtClientID.Size = new System.Drawing.Size(265, 22);
+            this.txtClientID.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 612);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Client ID:";
+            // 
+            // txtCallerID
+            // 
+            this.txtCallerID.Location = new System.Drawing.Point(199, 651);
+            this.txtCallerID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCallerID.Name = "txtCallerID";
+            this.txtCallerID.Size = new System.Drawing.Size(265, 22);
+            this.txtCallerID.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 644);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 31);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Caller ID:";
+            // 
+            // btnCreateIncident
+            // 
+            this.btnCreateIncident.Location = new System.Drawing.Point(199, 686);
+            this.btnCreateIncident.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCreateIncident.Name = "btnCreateIncident";
+            this.btnCreateIncident.Size = new System.Drawing.Size(267, 33);
+            this.btnCreateIncident.TabIndex = 15;
+            this.btnCreateIncident.Text = "Create Incident";
+            this.btnCreateIncident.UseVisualStyleBackColor = true;
+            // 
+            // btnNextForm
+            // 
+            this.btnNextForm.Location = new System.Drawing.Point(630, 463);
+            this.btnNextForm.Name = "btnNextForm";
+            this.btnNextForm.Size = new System.Drawing.Size(345, 206);
+            this.btnNextForm.TabIndex = 16;
+            this.btnNextForm.Text = "NEXT FORM";
+            this.btnNextForm.UseVisualStyleBackColor = true;
+            // 
+            // frmIncident
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1067, 734);
+            this.Controls.Add(this.btnNextForm);
+            this.Controls.Add(this.btnCreateIncident);
+            this.Controls.Add(this.txtCallerID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtClientID);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbxPriority);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpDateIssued);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rtbReportInfo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblHeading);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmIncident";
+            this.Text = "Incident";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbReportInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpDateIssued;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbxPriority;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtClientID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCallerID;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCreateIncident;
+        private System.Windows.Forms.Button btnNextForm;
+    }
+}
