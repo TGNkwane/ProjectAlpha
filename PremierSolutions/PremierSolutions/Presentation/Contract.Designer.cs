c3d226fb009ce29111543cd68d53f247835edf87
Tebogo Gerald Nkwane <nkwane.gerald@gmail.com>
2021-04-18 13:51:04 +0200

    Client Home Form Added And Working, Color Class Also Added

--- PremierSolutions/PremierSolutions/Presentation/Contract.Designer.cs
+++ PremierSolutions/PremierSolutions/Presentation/Contract.Designer.cs
@@ -181,6 +181,7 @@ namespace PremierSolutions
             this.btnNextForm.TabIndex = 75;
             this.btnNextForm.Text = "NEXT FORM";
             this.btnNextForm.UseVisualStyleBackColor = true;
+            this.btnNextForm.Click += new System.EventHandler(this.btnNextForm_Click);
             // 
             // Contract
             // 
---
+++ PremierSolutions/PremierSolutions/Presentation/colors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PremierSolutions
+{
+    class colors
+    {
+        public Color bgColor = Color.FromArgb(26, 26, 26);
+        public Color bgDarkerColor = Color.FromArgb(20, 20, 20);
+        public Color bgLighterColor = Color.FromArgb(30, 30, 30);
+
+        public Color accentColor = Color.FromArgb(102, 112, 233);
+        public Color hoverAccent = Color.FromArgb(21, 23, 58);
+        public Color accentDarkerColor = Color.FromArgb(72, 82, 203);
+        public Color accentColorLighterColor = Color.FromArgb(122, 132, 253);
+
+        public Color grey = Color.FromArgb(197, 197, 197);
+        public Color green = Color.FromArgb(0, 200, 0);
+        public Color hoverGreen = Color.FromArgb(49, 75, 49);
+        public Color red = Color.FromArgb(200, 0, 0);
+        public Color hoverRed = Color.FromArgb(85, 0, 0);
+
+    }
+}
